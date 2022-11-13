4824d36ef4f9e99fa3cb0c8c1f8ac6d39bb20281
applewonder <2535227255@qq.com>
2022-11-12 22:15:50 -0800

    Get Start

---
+++ judger/start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Steins Gate Start!"
+
+mkdir compiled
+cd compiled
+
+path=$1
+
+for file in $(ls $path);
+do 
+    if [ "${file##*.}" = "cpp" ]; then
+        echo "Now compile ${file}\n"
+        file_a=(basename $file .cpp)
+        echo "$file_a" >> filelist.txt
+        echo "\n" >> filelist.txt
+        g++ $f -o ./"${i%.cpp}"
+    fi
+done
+
+
+
+a.out <input.txt >ouput.txt
