b9dbc2f05032bab9f22ceb361acb333581237fcb
applewonder <2535227255@qq.com>
2022-11-13 18:01:33 -0800

    reconstruct

--- judger/run_code.cpp
+++ judger/run_code.cpp
@@ -37,27 +37,25 @@ class run_code {
             }
         }
 
-        void run_test() {
+        void run_test(int test_case) {
             int count_folder = 0;
             for (auto& f : std::filesystem::directory_iterator("./r_compile")) {
                 if (std::filesystem::is_directory(f)) {
                     count_folder ++;
                     std::string subpath = f.path();
                     
-                    for (int i = 0; i < 3; i ++) {
-                        std::string inputs = "./r_input/" + std::to_string(count_folder) +"/" + std::to_string(i) + ".txt";
-                        for (auto& test_file : std::filesystem::directory_iterator(subpath)) {
-                            std::string file_name = test_file.path();
-                            int down_pos = file_name.rfind("/");
-                            file_name = file_name.substr(down_pos + 1);
-                            std::string output_path = "./r_output/" + std::to_string(count_folder) + "/" + file_name + ".txt";
-                            std::string compile_command = test_file.path().string() + "<" + inputs + ">>" + output_path;
-                            system(compile_command.c_str());
-                            std::string give_back = "echo \"\n\" >> " + output_path;  
-                            system(give_back.c_str());
-                        }
+                    std::string inputs = "./r_input/" + std::to_string(count_folder) +"/" + std::to_string(test_case) + ".txt";
+                    for (auto& test_file : std::filesystem::directory_iterator(subpath)) {
+                        std::string file_name = test_file.path();
+                        int down_pos = file_name.rfind("/");
+                        file_name = file_name.substr(down_pos + 1);
+                        std::string output_path = "./r_output/" + std::to_string(count_folder) + "/" + file_name + ".txt";
+                        std::string compile_command = test_file.path().string() + "<" + inputs + ">" + output_path;
+                        system(compile_command.c_str());
+                        std::string give_back = "echo \"\n\" >> " + output_path;  
+                        system(give_back.c_str());
                     }
-                    
+                
                 }
             }
         }
